bffdf0a3a30e530fc7249fec43ff4b71220d57c0
tdameros <tomdamerose@gmail.com>
2024-10-01 17:00:52 +0200

    fix: remove specific type in utils

--- include/utils.h
+++ include/utils.h
@@ -15,8 +15,8 @@
 
 void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memset(void *s, int c, size_t n);
-char	*ft_strchr(const char *s, int32_t c);
-int32_t	ft_strcmp(const char *s1, const char *s2);
+char *ft_strchr(const char *s, char c);
+int	ft_strcmp(const char *s1, const char *s2);
 int	ft_atoi(const char *str);
 
 #endif
